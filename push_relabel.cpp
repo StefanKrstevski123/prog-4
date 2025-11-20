9260b86d525dcddd7a46df61d89d58f841430364
StefanKrstevski123 <krstevskistefan31@gmail.com>
2025-11-21 00:48:32 +0100

    rela

--- push_relabel.cpp
+++ push_relabel.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-using namespace std;//ssss
+using namespace std;
 
 void push(int u, int v, vector<int>& height, vector<int>& excess,
           vector<vector<int>>& capacity, vector<vector<int>>& flow,
